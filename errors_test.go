965e9cbb2e238fabae5297b2a50cde62b8c58024
wind85 <locci.carlo.1985@gmail.com>
2016-09-13 02:13:59 +0200

    reached over 90% test coverage

--- errors_test.go
+++ errors_test.go
@@ -8,6 +8,7 @@ import (
 func tester() *errors {
 	return New("call indirection")
 }
+
 func TestErrors(t *testing.T) {
 	e := New("custom error")
 	t.Log(e.Error())
@@ -15,4 +16,16 @@ func TestErrors(t *testing.T) {
 
 	wrapped := Wrap(fmt.Errorf("wrapped error\n"))
 	t.Log(wrapped())
+}
+
+func TestMsgToLong(t *testing.T) {
+	New("thi message is suppose to be very long really really long and it isn't really that long weel let's see")
+}
+
+func TestTooManyParmas(t *testing.T) {
+	New("thodisa", "fdsajkl", "flkasdll")
+}
+
+func TestCustomCallDepth(t *testing.T) {
+	New("ciao carlo", "-1")
 }
